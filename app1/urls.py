495631a1a54f33ce0e238e6c69c1e1f4e668331e
shubh730 <shubhi231305@gmail.com>
2022-10-15 11:49:29 +0530

    about page working fine

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,8 @@ from app1 import views
 
 urlpatterns = [
     path('home/',views.home,name="home"),
+    path('about/', views.about, name="about"),
+
     
 
 
--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(self):
     return render(self,'home.html')
+
+def about(self):
+    return render(self,'about.html')
     
