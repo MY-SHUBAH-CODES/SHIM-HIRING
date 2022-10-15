b41269a2323fc95636852e527e159b8bd556cc09
shubh730 <shubhi231305@gmail.com>
2022-10-15 12:51:40 +0530

    added contact

--- app1/urls.py
+++ app1/urls.py
@@ -5,6 +5,8 @@ from app1 import views
 urlpatterns = [
     path('home/',views.home,name="home"),
     path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+
 
     
 
